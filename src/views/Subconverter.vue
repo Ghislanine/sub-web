3d01fd5243197d17d21677003a4bcdbc916c552f
Ghislanine <95122491+Ghislanine@users.noreply.github.com>
2022-03-11 09:59:19 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -255,18 +255,17 @@ export default {
         customBackend: {
           "localhost:25500 本地版": "http://localhost:25500/sub?",
           "sub-beta.vercel.app": "https://sub-beta.vercel.app/sub?",
-          "subcon.dlj.tf(subconverter作者提供-稳定)":
-            "https://subcon.dlj.tf/sub?",
-          "api.dler.io(sub作者&lhie1提供-稳定)": "https://api.dler.io/sub?",
-          "api.wcc.best(sub-web作者提供-稳定)": "https://api.wcc.best/sub?",
+          "sub.xeton.dev(subconverter作者提供-稳定)":"https://sub.xeton.dev/sub?",
+          "api.dler.io(lhie1提供-稳定)": "https://api.dler.io/sub?",
+          "sub.maoxiongnet.com(猫熊提供-稳定)": "https://sub.maoxiongnet.com/sub?",
           "sub.id9.cc(品云转换)": "https://sub.id9.cc/sub?",
         },
         backendOptions: [
           { value: "http://localhost:25500/sub?" },
           { value: "https://sub-beta.now.sh/sub?" },
-          { value: "https://subcon.dlj.tf/sub?" },
+          { value: "https://sub.xeton.dev/sub?" },
           { value: "https://api.dler.io/sub?" },
-          { value: "https://api.wcc.best/sub?" },
+          { value: "https://sub.maoxiongnet.com/sub?" },
           { value: "https://sub.id9.cc/sub?" },
         ],
         remoteConfig: [
